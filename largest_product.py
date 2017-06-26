34b2d46a3bbae7c8ae3e53a2de95b33d138bc4cd
Zach-Houston <zhousto1@vols.utk.edu>
2017-06-26 16:33:59 -0500

    Add files via upload
    This solution was very rough, I went a little willy-nilly with if statements near the end as I was trying to figure out why one of my cases wasn't passing.

---
+++ largest_product.py
@@ -0,0 +1,37 @@
+def answer(xs):
+    total = 1
+    found = False
+    zero = False
+    negatives = []
+    if len(xs) == 1:
+        return str(xs[0])
+    for num in xs:
+        if num == 0:
+            zero = True
+            continue
+        if num > 0:
+            total = total * num
+            found = True
+        if num < 0:
+            found = True
+            negatives.append(num)
+            
+    if (len(negatives) % 2 == 0):
+        for n in negatives:
+            total *= n
+    elif(len(negatives) == 1):
+        if zero == True:
+            return str(0)
+        if total == 1:
+            return str(negatives[0])
+        return str(total)
+    else:
+        negatives.sort()
+        for n in negatives[:-1]:
+            total *= n
+    if total == 1 and found == False:
+        return str(0)
+    if total < 0 and zero == True:
+        return str(0)
+    return str(total)
+print str(answer([-43]))
